e9003972bb18170ff5089ad878fd11696f17f1cc
Jason Draper <jason@drapergeek.com>
2016-03-25 11:20:15 -0400

    Add a few more bookmarks

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -11,13 +11,38 @@ Bookmark.reopenClass({
   FIXTURES: [
     {
       id: 1,
-      url: 'http://github.com',
-      name: 'Github',
+      url: 'http://thoughtbot.com',
+      name: 'thoughtbot',
     },
     {
       id: 2,
+      url: 'https://github.com',
+      name: 'GitHub',
+    },
+    {
+      id: 3,
+      url: 'https://twitter.com',
+      name: 'Twitter',
+    },
+    {
+      id: 4,
+      url: 'https://goodreads.com',
+      name: 'Good Reads',
+    },
+    {
+      id: 5,
+      url: 'https://pragprog.com',
+      name: 'Pragmatic Programmers',
+    },
+    {
+      id: 6,
       url: 'https://yahoo.com',
       name: 'Yahoo',
+    },
+    {
+      id: 7,
+      url: 'http://phoenixframework.org',
+      name: 'Phoenix',
     },
   ]
 });
